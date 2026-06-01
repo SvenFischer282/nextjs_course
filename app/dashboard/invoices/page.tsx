a9f260e3fbad3cf8c0d8ed8c672b89628bca04d0
SvenFischer282 <81426840+SvenFischer282@users.noreply.github.com>
2026-06-01 22:30:30 +0200

    firt commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// @ts-expect-error -- CSS side-effect import is handled by Next.js
+import "./ui/global.css";
+import { inter } from "./ui/fonts";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
